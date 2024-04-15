8c43a8371c197b9b6a2b55f09a44c06bec9c1ea8
Devika Makan <108333894+devika2711@users.noreply.github.com>
2024-04-15 20:34:37 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -6,25 +6,25 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
 from tensorflow.keras.utils import to_categorical
 
-# Load the dataset
+
 train_data = pd.read_csv('sign_mnist_train.csv')
 test_data = pd.read_csv('sign_mnist_test.csv')
 
-# Extract labels and images
+
 train_labels = train_data['label'].values
 train_images = train_data.drop('label', axis=1).values
 test_labels = test_data['label'].values
 test_images = test_data.drop('label', axis=1).values
 
-# Normalize the images
+
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-# Reshape the data to fit the model
+
 train_images = train_images.reshape(-1, 28, 28, 1)
 test_images = test_images.reshape(-1, 28, 28, 1)
 
-# One-hot encode the labels
+
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
 
@@ -42,7 +42,7 @@ model = Sequential([
     Dense(24, activation='softmax')  # 24 classes for the letters A-Z (excluding J and Z)
 ])
 
-# Compile the model
+
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Train the model
